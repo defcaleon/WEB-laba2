27c2fafd24f8f02643b80825f0a68abe1287abe1
Nikita Parmon <nikitka.parmon@mail.ru>
2021-02-20 12:30:35 +0300

    add readme and start task

--- laba2.php
+++ laba2.php
@@ -10,6 +10,8 @@ try {
 		throw new Exception('empty text');
 	}
 
+    $myStr = preg_replace('/\s+/', ' ', $myStr);
+
 	$counter=0;
 	for($i=0;$i<strlen($myStr);$i++) 
 	{
@@ -20,13 +22,43 @@ try {
 		}
 	}
 
-	echo "num of o and O is $counter";
+	$strArr= explode(" ",$myStr);
+	echo "num of o and O is $counter <br />";
+
+	makeBigLetters($strArr);
+	output($strArr);
+
 }
 catch (Exception $e) {
 	echo $e->getMessage();
 }
 
 
+function makeBigLetters(&$arr){
 
+    $i=2;
+    while($i<count($arr)){
+
+        $arr[$i]=strtoupper($arr[$i]);
+        $i+=3;
+    }
+}
+
+function output(&$arr){
+    $color = 720371;
+
+    for($i=0;$i<count($arr);$i++){
+        for($j=0;$j<strlen($arr[$i]);$j++){
+            if($j % 3==2){
+                echo "<p style=color: #$color >$arr[$i][$j]</p>";
+
+            }else{
+                echo "<p>$arr[$i][$j]</p>";
+            }
+        }
+        echo " ";
+    }
+    echo "<br/>";
+}
 
 ?>
